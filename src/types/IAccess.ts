13c0ba178e8217e62cde424c0758750208d5b66b
Neel Patel <neelpatel.6531@gmail.com>
2022-01-12 21:19:01 +0530

    add access types

--- src/types/IAccess.ts
+++ src/types/IAccess.ts
@@ -4,5 +4,13 @@ export interface IAccess extends IBase {
   userId: string;
   laboratoryId: string;
   departmentId: string;
+  accessList: {
+    master: boolean;
+    customer: boolean;
+    accounting: boolean;
+    samples: boolean;
+    stokManagement: boolean;
+    laboratorySetting: boolean;
+  };
   role: 'ADMIN' | 'RECEPTIONIST' | 'CHEMIST' | 'ACCOUNT' | 'HOD';
 }
--- src/types/IDepartment.ts
+++ src/types/IDepartment.ts
@@ -3,6 +3,5 @@ import { IBase } from './IBase';
 export interface IDepartment extends IBase {
   name: string;
   description: string;
-  accessList: object;
   laboratoryId: string;
 }
