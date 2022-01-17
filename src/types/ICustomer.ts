3484225195911a2aacce08a51a26dfa6358d4c06
Neel Patel <neelpatel.6531@gmail.com>
2022-01-17 08:36:34 +0530

    add customer type

---
+++ src/types/ICustomer.ts
@@ -0,0 +1,27 @@
+import { IAddress, IBankDetails } from '..';
+import { IBase } from './IBase';
+
+interface IContactDetails {
+  name: string;
+  designation: string;
+  department: string;
+  email: string;
+  phone: string;
+  country: string;
+}
+
+export interface ICustomer extends IBase {
+  name: string;
+  email: string;
+  phone: string;
+  country: string;
+  address: IAddress;
+  contactDetails: IContactDetails[];
+  bankingDeatils: IBankDetails;
+  pan: string;
+  gst: string;
+  aadhar: string;
+  drugLicenceNumber: string;
+  paymentTerms: number;
+  creditLimit: number;
+}
