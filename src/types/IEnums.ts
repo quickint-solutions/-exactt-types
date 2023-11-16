2bf8af139a452d1b240cc4884dd73e0b0465d911
Yash Jani <yashjani3839@gmail.com>
2023-11-16 18:13:21 +0530

    add result types in enum

--- src/types/IEnums.ts
+++ src/types/IEnums.ts
@@ -11,4 +11,16 @@ export interface IEnums {
     FOOD_AND_WATER: 'Food & Water';
     PHARMACEUTICAL: 'Pharmaceutical';
   };
+  resultType: {
+    KSZ_Sample: 'ksz sample';
+    Food_Master: 'food master';
+    BSI_Report: 'bsi report';
+    W_PDW_AS_PER_Fassi: 'w pdw as per fassi';
+    W_General: 'w general';
+    PDW_General: 'pdw general';
+    PDW_AS_PER_Fassi: 'pdw as per fassi';
+    MUNDRA: 'mundra';
+    FOOD_General: 'food general';
+    Central_Fssai_Form_Vii_A: 'central fssai form vii a';
+  };
 }
