9f669680adf211b44127d1fe70cdff4be64d6d04
Yash Jani <yashjani3839@gmail.com>
2023-11-22 00:53:37 +0530

    changes on result types

--- src/types/IEnums.ts
+++ src/types/IEnums.ts
@@ -12,15 +12,15 @@ export interface IEnums {
     PHARMACEUTICAL: 'Pharmaceutical';
   };
   resultType: {
-    KSZ_SAMPLE: 'KSZ_SAMPLE';
-    FOOD_MASTER: 'FOOD_MASTER';
-    BSI_REPORT: 'BSI_REPORT';
-    W_PDW_AS_PER_FASSI: 'W_PDW_AS_PER_FASSI';
-    W_GENERAL: 'W_GENERAL';
-    PDW_GENERAL: 'PDW_GENERAL';
-    PDW_AS_PER_FASSI: 'PDW_AS_PER_FASSI';
-    MUNDRA: 'MUNDRA';
-    FOOD_GENERAL: 'FOOD_GENERAL';
-    CENTRAL_FSSAI_FORM_Vii_A: 'CENTRAL_FSSAI_FORM_Vii_A';
+    KSZ_SAMPLE: 'Ksz Sample';
+    FOOD_MASTER: 'Food Master';
+    BSI_REPORT: 'Bsi Report';
+    W_PDW_AS_PER_FASSI: 'W Pwd As Per Fassi';
+    W_GENERAL: 'W General';
+    PDW_GENERAL: 'Pdw General';
+    PDW_AS_PER_FASSI: 'Pdw As Per Fassi';
+    MUNDRA: 'Mundra';
+    FOOD_GENERAL: 'Food General';
+    CENTRAL_FSSAI_FORM_Vii_A: 'Central Fssai Form Vii_a';
   };
 }
