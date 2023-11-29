392b1b5b27e5460ee18ab8c882f74f90e28a80d6
Yash Jani <yashjani3839@gmail.com>
2023-11-29 10:16:09 +0530

    add fields in sample

--- src/types/ISample.ts
+++ src/types/ISample.ts
@@ -52,4 +52,7 @@ export interface ISample extends IBase {
   completeDate?: Date;
   reportDate?: Date;
   url: string;
+  reviewedBy: string;
+  authorizedSignature1: string;
+  authorizedSignature2?: string;
 }
