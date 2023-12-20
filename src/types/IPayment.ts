5d828fad6fa4e894acd6b36a1d10042c22792a7d
Yash3839 <yashjani3839@gmail.com>
2023-12-20 13:03:48 +0530

    changes  on payment and create new payment details

--- src/types/IPayment.ts
+++ src/types/IPayment.ts
@@ -1,13 +1,22 @@
 import { IBase } from './IBase';
 import { ICustomer } from './ICustomer';
+import { ILaboratory } from './ILaboratory';
+import { IReference } from './IReference';
 import { ISample } from './ISample';
 import { IUser } from './IUser';
 
 export interface IPayment extends IBase {
   customer: string | ICustomer;
+  reference: string | IReference;
   sample: string | ISample;
-  paymentMode: string | 'CASH ' | 'ONLINE ' | 'CHEQUE ' | 'UPI';
+  paymentMode:  'CASH ' | 'ONLINE ' | 'CHEQUE ' | 'UPI';
   collectedBy: string | IUser;
   amount: number;
   laboratoryId: string;
+}
+
+export interface IPaymentDetails extends IBase {
+  sample : string | ISample;
+  customer: string | ICustomer;
+  laboratory: string | ILaboratory;  
 }
